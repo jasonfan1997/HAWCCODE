4d4536aa49fb459c3a341cfb83c4a79a10bf701e
jasonfan1997 <fkt8356@gmail.com>
2018-08-30 15:37:26 +0800

    Update extract.py

--- addvariable/modeltrain/extract.py
+++ addvariable/modeltrain/extract.py
@@ -19,8 +19,7 @@ import pandas as pd
 
 GAMMADIR = "./Signal.csv"
 HADRONDIR="./recco_run007740.csv"
-COLUMNS = ["rec.nHit/U/1","rec.CxPE40/F/0.01","rec.PINC/F/0.01","rec.logNNEnergyV2/F/0","rec.disMax/F/0.01","rec.LDFAge/F/0.01"
-,"rec.LDFAmp/F/0.01","rec.LDFChi2/F/0.01","rec.nChAvail/U/1","rec.nHitSP20/U/1"]
+COLUMNS = ["rec.nHit/U/1","rec.CxPE40/F/0.01","rec.PINC/F/0.01","rec.logNNEnergyV2/F/0","rec.disMax/F/0.01","rec.LDFAmp/F/0.01","rec.LDFChi2/F/0.01","rec.nChAvail/U/1","rec.nHitSP20/U/1"]
 allset = pd.read_csv(GAMMADIR, skipinitialspace=True,
                              skiprows=0, usecols=COLUMNS)[COLUMNS].as_matrix()
 hadron = pd.read_csv(HADRONDIR, skipinitialspace=True,
@@ -30,10 +29,15 @@ allset=allset[allset[:,1]!=0,:]
 hadron=hadron[hadron[:,1]!=0,:]
 allset[:,0]= np.log10(np.divide(allset[:,1], allset[:,0],out=np.zeros_like(allset[:,0]), where=allset[:,0]!=0))
 allset=np.delete(allset,1,1)
+
+allset[:,6]=np.divide(allset[:,7], allset[:,6],out=np.zeros_like(allset[:,6]), where=allset[:,6]!=0)
+allset=np.delete(allset,7,1)
 allset=np.hstack((np.ones((len(allset),1)),allset))
-allset=allset[3908982:,:]
+#allset=allset[3908982:,:]
 hadron[:,0]= np.log10(np.divide(hadron[:,1],hadron[:,0], out=np.zeros_like(hadron[:,0]), where=hadron[:,0]!=0))
 hadron=np.delete(hadron,1,1)
+hadron[:,6]=np.divide(hadron[:,7], hadron[:,6],out=np.zeros_like(hadron[:,6]), where=hadron[:,6]!=0)
+hadron=np.delete(hadron,7,1)
 hadron = np.hstack((np.zeros((len(hadron),1)),hadron))
 hadron=hadron[:len(allset),:]
 allset=np.vstack((allset,hadron))
